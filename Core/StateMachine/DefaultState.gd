1f3d5b0988efe06407a278031c1dca768f74f57f
ala <aladdinalkhafaji@gmail.com>
2020-03-10 23:01:31 +0300

    Extract inner classes

---
+++ Core/StateMachine/DefaultState.gd
@@ -0,0 +1,7 @@
+extends "res://Core/StateMachine/State.gd"
+
+#Default state class to implement null object pattern for the state machine's current state
+
+func _process(delta): print("Unimplemented _process(delta)")
+func _physics_process(delta): print("Unimplemented _physics_process(delta)")
+func _input(event): print("Unimplemented _input(event)")
