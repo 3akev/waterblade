c8cb64a66586599462b3d8d4d5a42d72037e9cc1
ala <aladdinalkhafaji@gmail.com>
2020-03-10 22:45:01 +0300

    Add state machine addon

--- Core/Entities.gd
+++ Core/Entities.gd
@@ -4,9 +4,8 @@ func _init():
 	Events.connect("spawn_entity", self, "on_spawn_entity")
 
 func _ready():
-	Events.emit_signal("spawned_entity", $Player)
+	Events.emit_signal("spawn_entity", $Player)
 
 func on_spawn_entity(entity):
 	add_child(entity)
 	Events.emit_signal("spawned_entity", entity)
-
